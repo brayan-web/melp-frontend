deed6ddb7931011c3cb8a83703b6f1251c6e383c
brayan-web <brayan.mail.tester@gmail.com>
2023-12-26 13:07:39 -0600

    feat: create header components

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="py-10 bg-emerald-500 flex justify-between items-center">
+        <div class="font-medium text-3xl pl-2 text-white tracking-[7px]">MELP</div>
+    </div>
+</template>
